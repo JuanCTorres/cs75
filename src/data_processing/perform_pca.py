af6b84fd8d2d53abed57c1b883dad627b13f7dbc
FoxyGit <Jing.Geng.GR@dartmouth.edu>
2017-03-14 04:10:38 -0400

    modified pca

--- src/data_processing/perform_pca.py
+++ src/data_processing/perform_pca.py
@@ -1,24 +1,50 @@
 import os, re, sys
 import numpy as np
 from scipy import linalg
+from random import shuffle
 from read_data import read_preprocessed_data
+from split_data import SplitData
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
 from sklearn.model_selection import cross_val_score
 
 
 
-FEATURE_PATH = '../../data/aaindex/aaindex_used.txt'
+FEATURE_PATH = '../../data/aaindex/aaindex_all.txt'
 FEATURE_TO_FIT = 123
-FEATURE_KEPT = 20
+FEATURE_KEPT = 2
+
+INPUT_FILE = "../../data/animals/label_scores.txt"
+
+def cross_val_pca(Xtr):
+    n_samples, n_features = len(Xtr), len(Xtr[0])
+    min_max_scaler = preprocessing.MinMaxScaler(feature_range=(0, 100))
+    X_scaled = preprocessing.scale(min_max_scaler.fit_transform(np.array(Xtr)))
+    pca = PCA(svd_solver='full')
+    pca.fit(X_scaled)
+    print "Variance ration among dimensions: ", pca.explained_variance_ratio_
+
+
+    n_components = np.arange(0, n_features, 3)
+    pca_scores = []
+    for n in n_components:
+        pca.n_components = n
+        score = np.mean(cross_val_score(pca, Xtr, cv=5))
+        pca_scores.append(score)
+        print n, score
+    return pca_scores
+    pass
+
 
 def train_pca(Xtr):
-    #min_max_scaler = preprocessing.MinMaxScaler(feature_range=(0, 100))
-    #X_scaled = preprocessing.scale(min_max_scaler.fit_transform(np.array(Xtr)))
-    X_scaled = preprocessing.scale(np.array(Xtr))
-    pca = PCA(svd_solver='auto', n_components=FEATURE_TO_FIT)
+    min_max_scaler = preprocessing.MinMaxScaler(feature_range=(0, 100))
+    X_scaled = preprocessing.scale(min_max_scaler.fit_transform(np.array(Xtr)))
+    print 'Xtr: ', len(Xtr), 'Xtr[0]', len(Xtr[0])
+    #X_scaled = preprocessing.scale(np.array(Xtr))
+    FEATURE_TO_FIT = len(Xtr)
+    pca = PCA(svd_solver='full', n_components= len(Xtr[0]))
     pca.fit(X_scaled)
-    #print "Variance ration among dimensions: ", pca.explained_variance_ratio_
+    print "Variance ration among dimensions: ", pca.explained_variance_ratio_
     return pca
 
 # def save_transfromed_train_data(Ytr, Xtr, pca, label_socres_path):
@@ -58,6 +84,8 @@ if __name__ == '__main__':
         raise Exception('Please enter a valid dataset to use. Accepted: \'plants\' and \'animals\'')
 
     #Y, X = create_data(input_file)
-    #pca_preprocess(Y, X, data_folder)
+    # pca_preprocess(Y, X, data_folder)
+    Y, X = read_preprocessed_data(input_file, FEATURE_PATH, 5)
+    train_pca(Xtr=X)
 
     pass
